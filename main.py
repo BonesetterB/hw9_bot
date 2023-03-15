3f65fe508571b6dad798444c7d548bba333a85b0
BonesetterB <122158929+BonesetterB@users.noreply.github.com>
2023-03-15 10:59:58 +0200

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-memory=[]
+memory={}
 t=True
 def exit():
     global t
@@ -12,63 +12,60 @@ def command():
        'phone: this command shows the persons phone command shoude be look like this "change name number"']
     return '\n'.join(l)
 def add(string):
-    l=string.split(' ')
-    memory.append({l[1]:l[2]})
+    list_of_elements=string.split(' ')
+    memory[list_of_elements[1]]=list_of_elements[2]
     return 'Number was success add!'
 def phone(string):
-    l=string.strip()[6:]
-    for i in memory:
-        for k in filter(lambda x:x==l,i.keys()):
-            return i[k]
+    name_person=string.strip()[6:]
+    for k in filter(lambda x:x==name_person,memory.keys()):
+        return memory[k]
 
 def change(string):
-    l=string.split(' ') 
-    for i in memory:
-        for k in filter(lambda x:x==l[1],i.keys()):
-            i[k]==l[2]
+    list_of_elements=string.split(' ') 
+    for k in filter(lambda x:x==list_of_elements[1],memory.keys()):
+        memory[k]=list_of_elements[2]
+    return 'Number was success change!'
 def show_all():
-    l=[]
-    for i in memory:
-        for k, v in i.items():
-            l.append(f'{k}: {v}')
-    return '\n'.join(l)
+    list_of_numbers=[]
+    for k, v in memory.items():
+        list_of_numbers.append(f'{k}: {v}')
+    return '\n'.join(list_of_numbers)
 def check_comand(string):
-    def checks(name_command,list):
-        coman={'add':3,
-               'change':3,
-               'phone':2}
-        if len(list)==coman[name_command]:
-            if any(char.isdigit() for char in list[1])==False:
-                if len(list)==2:
-                    return phone(string)
-                else:
-                    if any(char.isdigit() for char in list[2])==False:
-                        return 'Give me  phone please without letter'
-                    else:
-                        return dict_1[x](string)
+    def checks(list):
+        comands_on_check={2:3,
+               3:3,
+               4:2}
+        if len(list)==comands_on_check[command_user]:
+            try:
+                if any(char.isdigit() for char in list[1])==False and any(char.isdigit() for char in list[2])==True:
+                    return dict_of_command[command_user](string)
+            except IndexError:
+                return phone(string)
             else:
-                return 'Enter name person not a number'
+                return 'The name must not contain numbers, and the number must not contain characters other than numbers'
+        elif len(list)>3:
+            return 'Remove spaces in the phone'
         else:
             return 'Give me name and phone please'
-    dict_1={1:exit,
+    dict_of_command={1:exit,
             0:command,
             2:add,
             4:phone,
             3:change,
             5:show_all}    
-    coommand=["command","exit",'add','change','phone','show_all']
-    j=string.split(' ')
+    posibl_coommand=["command","exit",'add','change','phone','show_all']
+    list_from_string=string.split(' ')
     try:
-        x=coommand.index(j[0].lower())
+        command_user=posibl_coommand.index(list_from_string[0].lower())
     except ValueError:
         return('You write wrong command, for  to get a list of commands write command')
     try:
-        return dict_1[x]()
+        return dict_of_command[command_user]()
     except TypeError:
-        return checks(j[0].lower(),j)
+        return checks(list_from_string)
 def main():
     global t
     while t:
-        x=input()
-        print(check_comand(x))
+        command_user=input()
+        print(check_comand(command_user))
 main()
