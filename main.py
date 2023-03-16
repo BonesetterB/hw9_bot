0e6363fb1e9758394e17ac7332f1745fb9c5023f
BonesetterB <122158929+BonesetterB@users.noreply.github.com>
2023-03-16 10:33:48 +0200

    p

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 memory={}
 
-# t=True
 
 def check_command(command):
     def wrapper(string):
@@ -16,8 +15,6 @@ def check_command(command):
 
 
 def exit(string):
-    # global t
-    # t=False
     return "Good bye!"
 
 
@@ -43,15 +40,11 @@ def add(string):
 def phone(string):
     name_person = string
     return memory[name_person]
-    # for k in filter(lambda x:x==name_person, memory.keys()):
-    #     return memory[k]
 
 @check_command
 def change(string):
     data =string.split(' ')
     memory[data[0]] = int(data[1])
-    # for k in filter(lambda x:x==list_of_elements[0],memory.keys()):
-    #     memory[k]=list_of_elements[1]
     return 'Number was success change!'
 
 
@@ -62,40 +55,6 @@ def show_all(string):
     return '\n'.join(list_of_numbers)
 
 
-# def check_comand(string):
-#     def checks(list):
-#         comands_on_check={2:3,
-#                3:3,
-#                4:2}
-#         if len(list)==comands_on_check[command_user]:
-#             try:
-#                 if any(char.isdigit() for char in list[1])==False and any(char.isdigit() for char in list[2])==True:
-#                     return dict_of_command[command_user](string)
-#             except IndexError:
-#                 return phone(string)
-#             else:
-#                 return 'The name must not contain numbers, and the number must not contain characters other than numbers'
-#         elif len(list)>3:
-#             return 'Remove spaces in the phone'
-#         else:
-#             return 'Give me name and phone please'
-#     dict_of_command={1:exit,
-#             0:command,
-#             2:add,
-#             4:phone,
-#             3:change,
-#             5:show_all}    
-#     posibl_coommand=["command","exit",'add','change','phone','show_all']
-#     list_from_string=string.split(' ')
-#     try:
-#         command_user=posibl_coommand.index(list_from_string[0].lower())
-#     except ValueError:
-#         return('You write wrong command, for  to get a list of commands write command')
-#     try:
-#         return dict_of_command[command_user]()
-#     except TypeError:
-#         return checks(list_from_string)
-    
 COMMANDS = {command: 'command',
             exit: 'exit',
             add: 'add',
@@ -112,7 +71,6 @@ def command_handler(text: str):
 
 
 def main():
-    # global t
     while True:
         command_user = input('>>>')
         command, data = command_handler(command_user)
